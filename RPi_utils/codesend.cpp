5f11a33afc57cafaef74729aed46c4a1cad3e6d0
Nick Gal <nickgal@me.com>
2015-10-28 18:48:00 -0700

    change pin number and set codesend pulse length to 189

--- RPi_utils/codesend.cpp
+++ RPi_utils/codesend.cpp
@@ -1,9 +1,9 @@
 /*
 
  'codesend' hacked from 'send' by @justy
- 
+
  - The provided rc_switch 'send' command uses the form systemCode, unitCode, command
-   which is not suitable for our purposes.  Instead, we call 
+   which is not suitable for our purposes.  Instead, we call
    send(code, length); // where length is always 24 and code is simply the code
    we find using the RF_sniffer.ino Arduino sketch.
 
@@ -14,25 +14,25 @@
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
-     
+
 
 int main(int argc, char *argv[]) {
-    
+
     // This pin is not the first pin on the RPi GPIO header!
     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
     // for more information.
-    int PIN = 0;
-    
+    int PIN = 7;
+
     // Parse the firt parameter to this command as an integer
     int code = atoi(argv[1]);
-    
+
     if (wiringPiSetup () == -1) return 1;
-	printf("sending code[%i]\n", code);
-	RCSwitch mySwitch = RCSwitch();
-	mySwitch.enableTransmit(PIN);
-    
+    printf("sending code[%i]\n", code);
+    RCSwitch mySwitch = RCSwitch();
+    mySwitch.setPulseLength(189);
+      
     mySwitch.send(code, 24);
-    
+
 	return 0;
 
 }
